cee10bad7ee89e7f0b11da3bdc18cab8917ee9a2
iyogeshmore <iyogesh.more1998@gmail.com>
2022-04-18 14:01:54 +0530

    Added bike model

---
+++ Bike.java
@@ -0,0 +1,28 @@
+package Bridgelabz;
+
+import java.util.ArrayList;
+
+public class Bike {
+    public static void main(String[] args) {
+        ArrayList<String>bike_list = new ArrayList<String>(3);
+        bike_list.add("Suzuki");
+        bike_list.add("Yamaha");
+        bike_list.add("Kawasaki");
+        bike_list.add("Ninja");
+        System.out.println("*** Bike list ***");
+        for (int i=0;i<3;i++){
+            System.out.println(bike_list.get(i).toString());
+        }
+        ArrayList<String>oldmodel = new ArrayList<String>(2);
+        oldmodel.add("TVS");
+        oldmodel.add("Hero");
+        oldmodel.add("Honda");
+        bike_list.addAll(oldmodel);
+        System.out.println("New size of the list is :" +bike_list.size());
+        System.out.println("*** Bike list ***");
+        for (int i=0;i<7;i++)
+        {
+            System.out.println(bike_list.get(i).toString());
+        }
+    }
+}
